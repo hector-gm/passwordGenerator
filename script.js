96e71f12b2e6b158afe7d8ed5585c033bc17844a
Hector Garcia Millares <hgarcia.millares@gmail.com>
2020-10-20 22:10:00 -0400

    Updated .js file with reworked randomSelection function

--- script.js
+++ script.js
@@ -13,16 +13,66 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-//We define the character sets acceptable for our password
-var validNumber = Math.floor(Math.random() * 10); //Will return a random number between 0 and 9 (both included)
-
-//We define the generatePassword function first listed in line 6
 function generatePassword () {
-  var length = prompt("Set the password length with a number of characters between 8 and 128");
-  var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789~!@#$%^&*";
-  var randomSelection = "";
-  for (var i = 0, n = charset.length; i < length; i++) {
-    randomSelection += charset.charAt(Math.floor(Math.random() * n));
+  //We define the optional character sets for each prompt to be presented to the user   
+  var lower = "abcdefghijklmnopqrstuvwxyz";
+  var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numer = "0123456789";
+  var symbol = "~!@#$%^&*,?";
+
+  //User is prompted for length of the password
+  var passwordLength = parseInt(prompt ("How many characters long do you need your password to be?"))
+  //Valid password length is set between 8 and 128 characters (inclusive)
+  if (passwordLength<8 || passwordLength>128) {
+      alert("Valid password length is at least 8 characters and maximum 128 characters. "+
+          "Please try again")
   }
-  return randomSelection;
+  else {
+      console.log("Selected password length is: "+passwordLength);
+      //User is prompted to confirm lowercase letters are to be used. Answer is logged
+      var confirmLower = confirm ("Want to include lowercase letters?");
+      console.log("User is including lowercase characters:"+confirmLower);
+      //User is prompted to confirm uppercase letters are to be used. Answer is logged
+      var confirmUpper = confirm ("Want to include uppercase letters?");
+      console.log("User is including uppercase characters:"+confirmUpper);
+      //User is prompted to confirm numbers are to be used. Answer is logged
+      var confirmNumeric = confirm ("Want to use numbers?");
+      console.log("User is including numeric characters:"+confirmNumeric);
+      //User is prompted to confirm symbols are to be used. Answer is logged
+      var confirmSymbols = confirm ("Should we include symbols?");
+      console.log("User is including symbols:"+confirmSymbols);
+  }     
+  
+  //Initially the character set is empty until the user sets the valid sets to include and use in our randomSelection function
+
+  //The valid character set to be used in the generatePassword function will depend on the following code
+  //Maximum character set to be used is "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789~!@#$%^&*";
+   
+  var characterSet = ""; 
+  
+  if (confirmLower){
+      characterSet+=lower;
+  }
+
+  if (confirmUpper){
+      characterSet+=upper;
+  }
+
+  if (confirmNumeric){
+      characterSet+=numer;
+  }
+
+  if (confirmSymbols){
+      characterSet+=symbol;
+  }
+  console.log(characterSet);
+
+  //Random selection from character strings is defined below
+  
+  var randomSelection = "";
+   for (var i=0; i<passwordLength; i++){
+     randomSelection += characterSet.charAt(Math.floor(Math.random() * characterSet.length));
+   }
+   console.log(randomSelection);
+   return randomSelection;     
 }
